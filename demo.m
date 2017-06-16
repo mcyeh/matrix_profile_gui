61f0822d2df1ec73a71cecd0e9322fd0094f135d
Anh <anhdau173@gmail.com>
2017-06-15 18:38:53 -0700

    Add a demo for annotation vector framework

--- demo.m
+++ demo.m
@@ -8,3 +8,9 @@ matrixProfileTime = tic();
 [matrixProfile, profileIndex, motifIndex, discordIndex] = ...
     interactiveMatrixProfile(data, subLen);
 matrixProfileTime = toc(matrixProfileTime);
+
+%% annotation vector framework demo
+load('test_av_data');
+av = make_AV_complexity(data, subLen);
+tic;[matrixProfile, profileIndex, motifIndex, discordIndex] = ...
+    interactiveMatrixProfile(data, subLen, av); toc; 
