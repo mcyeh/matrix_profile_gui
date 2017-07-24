95573baac471c949a46db86dea3527f363634712
Mike Yeh <myeh003@ucr.edu>
2017-07-24 02:43:34 -0700

    update the demo to show the new function

--- demo.m
+++ demo.m
@@ -4,7 +4,18 @@ clc;
 %% load test data
 load('testData.mat');
 
-matrixProfileTime = tic();
-[matrixProfile, profileIndex, motifIndex, discordIndex] = ...
-    interactiveMatrixProfile(data, subLen);
-matrixProfileTime = toc(matrixProfileTime);
+%%
+% without constrain, the seoncd and third pattern are the motif pair
+matrixProfileTime1 = tic();
+[matrixProfile1, profileIndex1, motifIndex1, discordIndex1] = ...
+    interactiveMatrixProfile(data, subLen, inf); 
+matrixProfileTime1 = toc(matrixProfileTime1);
+
+%%
+% by forcing the algorithm to cross a certain point, the first and third 
+% pattern become the motif pair
+matrixProfileTime2 = tic();
+[matrixProfile2, profileIndex2, motifIndex2, discordIndex2] = ...
+    interactiveMatrixProfile(data, subLen, 2800);
+matrixProfileTime2 = toc(matrixProfileTime2);
+
